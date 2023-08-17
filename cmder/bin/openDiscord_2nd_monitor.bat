697f5853533dc92a3a7bafdaa2750a5e531bd100
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-08-17 21:44:13 +0300

    # edit paths in cmder\bin\openDiscord_2nd_monitor.bat and update readme.md#

--- cmder/bin/openDiscord_2nd_monitor.bat
+++ cmder/bin/openDiscord_2nd_monitor.bat
@@ -3,5 +3,5 @@ REM this .bat file is to run discord then move it to verticall monitor (rest of 
 REM I dont run discrod directly instead i call .ps1 script to run it to not link discord to current opened cmder 
 REM this can be done to any app i want to open in specific window
 
-powershell.exe -ExecutionPolicy Bypass -File C:\Users\OmarPc\repo_CPP_COMP_study\cmder/bin/openDiscord.ps1 && timeout /t 2 > nul
+powershell.exe -ExecutionPolicy Bypass -File C:\Users\OmarPc\repo_My_configs_cmder\cmder/bin/openDiscord.ps1 && timeout /t 2 > nul
 
