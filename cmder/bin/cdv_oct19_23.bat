6c23ebfb84f831e5d515d419764f8382d6d2f084
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-10-27 23:01:14 +0200

    # update cmder aliases: bard bot aliase now open bard on operagx not brave or chrome + `disweb` alias now opens discord in operagx also(edited the .bat file of this alias not alias itself)#

--- cmder/bin/cdv_oct19_23.bat
+++ cmder/bin/cdv_oct19_23.bat
@@ -33,20 +33,20 @@ SETLOCAL EnableDelayedExpansion ENABLEEXTENSIONS
 
 
 @REM ~START OF THE BATCH SCRIPT~
-
 @REM Debug & logging mode ? only on if %3 == -1 ( use 'if defined is_debug'  anywhere before any echo that is used for logging/debugging )
 set /A is_debug=1
 @REM check if third shell arg -currently only used to enable debug- is not set first to elemenate 'missing operand' msg in console
 if [%3] NEQ [] (
 	if "%3" NEQ "-1" ( set /A is_debug= ) else ( echo [101m [DEBUG MODE IS ON] [0m && ECHO ON )
-)
+) else ( set /A is_debug= 0  )
 
 @REM getting path arg and extra options arg
 set  fstPoShellArg=%1
-
+set /A is_empty=1
 if [%1] NEQ [] (
 	set fstPoShellArg=!fstPoShellArg:/=\!
 	set fstPoShellArg=!fstPoShellArg:"=!
+	set /A is_empty=0
 )
 
 if "!fstPoShellArg!"=="-i" (
@@ -61,7 +61,7 @@ if "!fstPoShellArg!"=="-i" (
 			if "!fstPoShellArg!"=="-h" (
 				set "toCheckPath=%CD%" && set "command_options=-h"
 			) else ( 
-				if [!fstPoShellArg!]==[] (
+				if !is_empty!==1 (
 					set "toCheckPath=%CD%" && set "command_options=%2"
 				) else (
 					set "toCheckPath=!fstPoShellArg!" && set "command_options=%2" 
@@ -71,8 +71,7 @@ if "!fstPoShellArg!"=="-i" (
 	)
 )
 
-@REM it's safer with quotes! but REMOVE any double quote in the path 
-@REM then append and prepend double quotes to the path just before using normal cd  command at end of the process
+@REM it's safer with quotes! but REMOVE any double quote in the path then append and prepend double quotes to the path just before using normal cd  command at end of the process
 set toCheckPath=!toCheckPath:"=!
 
 @REM replace all forward slashes with back slashes 
@@ -81,6 +80,18 @@ set toCheckPath=!toCheckPath:/=\!
 @REM always make last char to be the backslash "\"
 set "lastChar=!toCheckPath:~-1!"
 if not "!lastChar!"=="\" ( set "toCheckPath=!toCheckPath!\" )
+
+@REM -----------------------------------------------
+echo TESTINGG  all vars list : 
+echo 1) %1 %2 %2
+echo 2) is debug? !is_debug!
+echo 3) is first post shell arg empty(1==true is empty)? !is_empty!
+echo 4) !fstPoShellArg!
+echo 5) !CD!
+echo 6) !toCheckPath!
+echo 7) !command_options!
+echo END TESTING 
+@REM -----------------------------------------------
 
 if "!command_options!"=="-1" (
 	@REM disable all cdv effects: only deactivate python venv then  normally change dir via 'cd' to the provided path ( 
@@ -110,7 +121,7 @@ if "!command_options!"=="-h" (
 	echo.
 	echo [96m 3. [0m "check if there is exist python venv for it : if there is activate it , if not make the it then activate it"
 	echo.
-	echo [96m 4. [0m "to use it easly add `cdv.bat` folder path to your path variable in system environmental variables" "(in release version installer will add it auto and tool path will be at C:\Users\USERNAME\cdv\ )"
+	echo [96m 4. [0m "to use it easly add `cdv.bat` folder path to your path variable in system environmental variables (in release version installer will add it auto and tool path will be at C:\Users\USERNAME\cdv\ )"
 	echo.
 	echo [96m 5. [0m "if used `cdv -i` or `cdv <path> -i` : initialize .is_autoVenv then make new venv for current dir or provided path then activate the venv"
 	echo.
@@ -127,9 +138,7 @@ if "!command_options!"=="-h" (
 	echo [91m ~
 	echo [93m IMPORTANT: [0m "(DONT USE FOLDER NAMES OR PATHS WITH SPACES!)"
 	echo.
-	echo [0m "the command mostly used inside aliases of cd to repoes" 
-	echo.
-	echo [0m "( other wise make its default to disable extra modification and do normal cd unless ^%2 is set to "1")"
+	echo [0m "the command mostly used inside aliases of cd to repoes ( other wise make its default to disable extra modification and do normal cd unless ^%2 is set to "1")"
 	echo.
 	echo [93m "[works/tested on windows10 OS. can be used like this: `cdv path_arg <toggle_venv_arg>`]"[0m
 	echo [91m ~ [0m
@@ -138,10 +147,10 @@ if "!command_options!"=="-h" (
 	exit /b 0
 )
 
-
-if exist "!toCheckPath!" ( 
+if  exist "!toCheckPath!" ( 
 
 	if "!command_options!"=="-d" ( 
+		
 	
 		set /p is_confirmed=type 'y' if you sure to delete the venv of this dir^? all auto venv configs and python libs inside the venv will be deleted. choose (y / n^)  || set is_confirmed=n
 		
@@ -150,19 +159,57 @@ if exist "!toCheckPath!" (
 			echo "deleting (auto venv configs / cdv configs / all venv ) for this dir ..."
 			
 									
-			ENDLOCAL toCheckPath
+			ENDLOCAL
 			@REM deactivate 
 			if defined VIRTUAL_ENV ( call %VIRTUAL_ENV%\Scripts\deactivate.bat )
 			
 			setlocal EnableDelayedExpansion
 			
-			set /p venv_name=< !toCheckPath!.is_autoVenv
-						
+			if [%2]==[] (
+				if %1==-i (
+					set "toCheckPath=%CD%" 
+				) else (
+					if %1==-1 (
+						set "toCheckPath=%CD%"
+					) else ( 
+						if %1==-d (
+							set "toCheckPath=%CD%"
+						) else (
+							if %1==-h (
+								set "toCheckPath=%CD%"
+							) else ( 
+								if [%1]==[] (
+									set "toCheckPath=%CD%"
+								) else (
+									set "toCheckPath=%1"
+								)
+							)
+						)
+					)
+				)
+			) else (
+				set toCheckPath=%1
+			)
+
+			@REM it's safer with quotes! but REMOVE any double quote in the path then append and prepend double quotes to the path just before using normal cd  command at end of the process
+			set toCheckPath=!toCheckPath:"=!
+
+			@REM replace all forward slashes with back slashes 
+			set toCheckPath=!toCheckPath:/=\!
+
+			@REM always make last char to be the backslash "\"
+			set "lastChar=!toCheckPath:~-1!"
+			if not "!lastChar!"=="\" ( set "toCheckPath=!toCheckPath!\" )
+
+			set /p venv_name=< "!toCheckPath!.is_autoVenv"
+			
+
+
 			@REM delete venv folder
-			if exist "C:\Users\%USERNAME%\py_envs\!venv_name!" ( rd /S C:\Users\%USERNAME%\py_envs\!venv_name!)
+			if exist "C:\Users\%USERNAME%\py_envs\!venv_name!" ( rd /S C:\Users\%USERNAME%\py_envs\!venv_name! )
 					
 			@REM delete .is_autoVenv
-			if exist "!toCheckPath!.is_autoVenv" (rm -f !toCheckPath!.is_autoVenv && echo Done.)
+			if exist "!toCheckPath!.is_autoVenv" ( rm -f "!toCheckPath!.is_autoVenv" && echo Done. )
 						
 			@REM clean temp files and temp vars
 			set toCheckPath=
@@ -173,19 +220,23 @@ if exist "!toCheckPath!" (
 		
 		exit /b 0
 	)
+	
 	if "!command_options!"=="-i" ( 
-		type nul > !toCheckPath!.is_autoVenv
-			@REM if it's a git repo add this file to ignored files
-		if exist "!toCheckPath!.git\" ( echo .is_autovEnv >> !toCheckPath!.gitignore )
+		type nul > "!toCheckPath!.is_autoVenv"
+		@REM if it's a git repo add this file to ignored files
+		if exist "!toCheckPath!.git\" ( echo .is_autovEnv >> "!toCheckPath!.gitignore" )
 	)
 	
 	@REM leaving the backslash at end of path will make it harder to use '~' or get dir name using other methods. so remove it!
-	set toCheckPath=!toCheckPath:~0,-1!
+		@REM (it's assured to be always the last char because we appended it our selfs before and removed any double quotes)
+		set toCheckPath=!toCheckPath:~0,-1!
 
-	for /d %%p in (!toCheckPath!) do (
+
+	for /d %%p in ( "!toCheckPath!" ) do (
 		set "dir_name=%%~np" 
 		@REM actually ~nx gets the name with extention in case of path was file not dir path.'~n' is an  extractor or slicing operator must work inside for loop
 	)
+	
 	
 	if exist "!toCheckPath!\.is_autoVenv" (
 		@REM NOTE: <optional> append "_venv" to end of your proj folder name. This is optional step. This would be your venv folder name for this project or repo
@@ -213,7 +264,7 @@ if exist "!toCheckPath!" (
 		)
 				
 		@REM save venv name inside '.is_autoVenv' file
-		echo !venv_dir_name! > !toCheckPath!\.is_autoVenv
+		echo !venv_dir_name! > "!toCheckPath!\.is_autoVenv"
 			
 		set final_venv_active_path=C:\Users\%USERNAME%\py_envs\!venv_dir_name!\Scripts\activate
 		
@@ -232,9 +283,19 @@ if exist "!toCheckPath!" (
 	) else (
 		ENDLOCAL
 		set fstPoShellArg=%1
-		if [%fstPoShellArg%] NEQ [] set fstPoShellArg=%fstPoShellArg:"=%
-		@REM ENDLOCAL
-		if [%fstPoShellArg%]==[] (echo %CD%) else ( if not "%fstPoShellArg%"=="-1" ( if not "%fstPoShellArg%"=="-i" ( if not "%fstPoShellArg%"=="-d" (cd /d "%fstPoShellArg%") ) ) )
+		
+		if [%fstPoShellArg%]==[] ( 
+			echo %CD% 
+			) else ( 
+				if not %fstPoShellArg%==-1 (
+					 if not %fstPoShellArg%==-i (
+						 if not %fstPoShellArg%==-d (
+							 cd /d "%fstPoShellArg%" 
+						) 
+					) 
+				)
+			)
+
 		@REM cd works after ending localisation  so new dir in %CD% is in global scope not local
 		exit /b 0
 	)	
@@ -247,18 +308,28 @@ if exist "!toCheckPath!" (
 )
 
 
-
 if defined VIRTUAL_ENV ( call %VIRTUAL_ENV%\Scripts\deactivate.bat )
-if defined final_venv_active_path (call %final_venv_active_path%)
+if defined final_venv_active_path ( call %final_venv_active_path% ) 
 if exist "C:\Users\%USERNAME%\final_venv_active_path.txt" ( rm -f C:\Users\%USERNAME%\final_venv_active_path.txt )
 
 :normal_cd
 @REM finally cd to your project folder and exit :)!
 
 @REM ENDLOCAL
+
 set fstPoShellArg=%1
-if [%fstPoShellArg%] NEQ [] set fstPoShellArg=%fstPoShellArg:"=%
-if [%fstPoShellArg%]==[] (echo %CD%) else ( if not "%fstPoShellArg%"=="-1" ( if not "%fstPoShellArg%"=="-i" ( if not "%fstPoShellArg%"=="-d" (cd /d "%fstPoShellArg%") ) ) )
+
+if [%fstPoShellArg%]==[] ( 
+	echo %CD% 
+	) else ( 
+		if not %fstPoShellArg%==-1 (
+				if not %fstPoShellArg%==-i (
+					if not %fstPoShellArg%==-d (
+						cd /d "%fstPoShellArg%" 
+				) 
+			) 
+		)
+	)
 @REM cd works after ending localisation  so new dir in %CD% is in global scope not local
 exit /b 0
 
