555dbf712b6f1216e7e7d1f4fc973c865d9c24fe
Omar Rashad <77698890+orsnaro@users.noreply.github.com>
2024-03-16 05:32:25 +0200

    Update save_N_push_solved_prob.bat: done testing now code more usable for other users

--- cmder/bin/save_N_push_solved_prob.bat
+++ cmder/bin/save_N_push_solved_prob.bat
@@ -15,7 +15,7 @@ cd C:\Users\%USERNAME%\repo_CPP_COMP_study\
 
 
 rem Specify the folder to count the files in. (makes it if not present first)
-set probsFolder=tst_probs\
+set probsFolder=done_probs\
 if not exist "%probsFolder%" mkdir "%probsFolder%"
 
 
@@ -24,12 +24,12 @@ set /a FILECOUNT=0
 for /f %%a in ('dir /b /a-d-s-h "%probsFolder%"') do set /a FILECOUNT+=1
 
 
-echo solution file: %src_file%.%ext%
+rem note: change the file CP.cpp to one that you solve problems in it 
+copy %src_file%.%ext%  %probsFolder%prob_no_%FILECOUNT%.%ext%
+echo solution file: prob_no_%FILECOUNT%.%ext%
+
 rem enter the link of problem to be added to git commit message (optional)
 set /p commitMsg=paste problem link: || set commitMsg=NoLinkProvided
-
-rem note: change the file CP.cpp to one that you solve problems in it 
-copy %src_file%.%ext%  %probsFolder%\prob_no_%FILECOUNT%.%ext%
 
 rem add, commit and push your new solved problem in its own file (optional: leave problem url in the commit message)
 git add .\%src_file%.%ext% .\%probsFolder% & git commit -m "#Done: %commitMsg% #" & git push
