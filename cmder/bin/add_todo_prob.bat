9ae8b1bf061b0b550ba48e1db30d8425d45b0663
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2024-03-16 05:44:09 +0200

    # done testing add to do script and push solved problem script to be more usable for others#

--- cmder/bin/add_todo_prob.bat
+++ cmder/bin/add_todo_prob.bat
@@ -5,6 +5,9 @@ rem assign this bat file to an cmder alias  so you can with one command save the
 setlocal
 set oldpath=%cd%
 
+rem your code file without the extenstion (write ur code in constant name file e.g. Cp.cpp)
+set src_file=Cp
+
 set ext=%1
 
 rem change your repo path here
@@ -13,22 +16,24 @@ cd C:\Users\%USERNAME%\repo_CPP_COMP_study\
 
 rem Specify the folder to count the files in. (make it if not present first)
 set todoFolder=todo_probs\
+if not exist "%todoFolder%" mkdir "%todoFolder%"
 
 rem Get the number of files in the folder.
 set /a FILECOUNT=0
 for /f %%a in ('dir /b /a-d-s-h "%todoFolder%"') do set /a FILECOUNT+=1
 
 
+rem change the file that you solve CP in it  ( -1 one for the readme.md that is counted once as a a problem code )
+set /a FILECOUNT= %FILECOUNT% - 1
+copy %src_file%.%ext%  %todoFolder%prob_no_%FILECOUNT%.%ext%
+
+echo todo problem file: prob_no_%FILECOUNT%.%ext%
+
 rem enter the link of problem to be added to git commit message (optional)
 set /p commitMsg=paste problem link:  || set commitMsg=NoLinkProvided
 
-
-rem change the file that you solve CP in it  ( -1 one for the readme.md that is counted once as a a problem code )
-set /a FILECOUNT= %FILECOUNT% - 1
-copy Cp.%ext%  %todoFolder%\prob_no_%FILECOUNT%.%ext%
-
 rem add commit then push the new problem in todo folder
-git add .\Cp.%ext% .\todo_probs\ & git commit -m "#todo: %commitMsg% #" & git push
+git add .\%src_file%.%ext% .\todo_probs & git commit -m "#todo: %commitMsg% #" & git push
 
 
 cd %oldpath%
--- cmder/bin/save_N_push_solved_prob.bat
+++ cmder/bin/save_N_push_solved_prob.bat
@@ -1,29 +1,38 @@
 @echo off
 rem assign this bat file to an cmder alias  so you can with one command save the new problem you solved and push it to the remote repo 
+rem commands example: save_N_push_solved_prob.bat cpp   (this could be aliased for shorter command)
 
 setlocal
 set oldpath=%cd%
+
+rem your code file without the extenstion (solve in constant name file e.g. Cp.cpp)
+set src_file=Cp
+
 set ext=%1
 
 rem change your repo path here
 cd C:\Users\%USERNAME%\repo_CPP_COMP_study\
 
 
-rem Specify the folder to count the files in. (make it if not present first)
+rem Specify the folder to count the files in. (makes it if not present first)
 set probsFolder=done_probs\
+if not exist "%probsFolder%" mkdir "%probsFolder%"
+
 
 rem Get the number of files in the folder.
 set /a FILECOUNT=0
 for /f %%a in ('dir /b /a-d-s-h "%probsFolder%"') do set /a FILECOUNT+=1
 
-rem enter the link of problem to be added to git commit message (optional)
-set /p commitMsg=paste problem link:  || set commitMsg=NoLinkProvided
 
 rem note: change the file CP.cpp to one that you solve problems in it 
-copy Cp.%ext%  %probsFolder%\prob_no_%FILECOUNT%.%ext%
+copy %src_file%.%ext%  %probsFolder%prob_no_%FILECOUNT%.%ext%
+echo solution file: prob_no_%FILECOUNT%.%ext%
+
+rem enter the link of problem to be added to git commit message (optional)
+set /p commitMsg=paste problem link: || set commitMsg=NoLinkProvided
 
 rem add, commit and push your new solved problem in its own file (optional: leave problem url in the commit message)
-git add .\Cp.%ext% .\done_probs\ & git commit -m "#Done: %commitMsg% #" & git push
+git add .\%src_file%.%ext% .\%probsFolder% & git commit -m "#Done: %commitMsg% #" & git push
 
 
 cd %oldpath%
