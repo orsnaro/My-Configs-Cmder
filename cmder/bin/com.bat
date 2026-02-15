bb798423a91c0810aeabec97c5afa46453541b53
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2026-02-15 16:18:34 +0200

    enhance git commit batch script com.bat

--- cmder/bin/com.bat
+++ cmder/bin/com.bat
@@ -1,13 +1,16 @@
 @echo off
 if "%~2"=="" (
 	set "TITLE=%~1"
-    git commit -m "%TITLE%"
-	exit /b 0
+	
 )
 
-set "TITLE=%~1"
-set "BODY=%~2"
+if not "%~2"=="" (
+	set "TITLE=%~1"
+	set "BODY=%~2"
+)
 
 git commit -m "%TITLE%" -m "%BODY%"
+set TITLE=
+set BODY=
 exit /b 0
 
